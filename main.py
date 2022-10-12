83bfacfc1d85437cd507000f250745f88dfdb909
Arjun Darbha <arjunsdarbha@gmail.com>
2022-10-12 13:48:15 -0400

    DVS-XRAY #16 Added functionality to check required fields

--- main.py
+++ main.py
@@ -37,26 +37,38 @@ def connect_to_db(db_name: str) -> Union[None, str]:
 
 # Sidebar selection
 add_selectbox = st.sidebar.selectbox(
-    "Select DB: ",
-    ("None", "DVS Analytics", "DVS Training", "Mayo Clinic")
+        "Select DB: ",
+        ("None", "DVS Analytics", "DVS Training", "Mayo Clinic")
 )
 
 db_connection_name = None
 if add_selectbox != 'None':
     db_connection_name = connect_to_db(add_selectbox)
 
-
 # Add tabs
 tab_player, tab_score, tab_report, tab_x_ray, tab_compare, tab_logout = st.tabs(
         ["Player", "Score", "Report", "X-RAY", "Compare", "Logout"])
 
+
 # Player tab
+def check_required_fields(*args):
+    """
+    If any of the arguments is None, returns 0 else 1
+    :param args:
+    :return:
+    """
+    if 'None' in args:
+        return 0
+    else:
+        return 1
+
+
 with st.expander("Add new player"):
     with st.form(key="add_new_player"):
-        first_name = st.text_input(label="First name*")
-        last_name = st.text_input(label="Last name*")
-        birthdate = st.date_input(label="Birthdate*")
-        email = st.text_input(label="Email*")
+        first_name = st.text_input(label="First name*", value=None)
+        last_name = st.text_input(label="Last name*", value=None)
+        birthdate = st.date_input(label="Birthdate*", value=None)
+        email = st.text_input(label="Email*", value=None)
         trainer = st.selectbox(label="Trainer*", options=get_trainer_list(db_connection_name))
         facility = st.selectbox(label="Facility*", options=get_facility_list(db_connection_name))
         organization = st.selectbox(label="Organization*", options=get_org_list(db_connection_name))
@@ -64,21 +76,25 @@ with st.expander("Add new player"):
         position = st.selectbox(label="Position", options=["Starter", "Reliever"])
         throws = st.selectbox(label="Throws", options=["Left", "Right"])
         workout = st.selectbox(label="Workout*", options=get_workout_list(db_connection_name))
-        phone = st.text_input(label="Phone", max_chars=10)
+        phone = st.text_input(label="Phone", value=None, max_chars=10)
 
         st.text('*Required')
 
         submit_form = st.form_submit_button(label="SUBMIT")
 
-
-
-
+        if submit_form:
+            req_fields = check_required_fields(first_name, last_name, birthdate,
+                                               email, trainer, facility, organization, team,
+                                               workout)
+            if not req_fields:
+                st.error('All required fields must be entered')
+            else:
+                st.success('Form is successfully submitted')
 
 with st.expander('Edit existing player'):
     st.text('Edit existing player')
 
     df = pd.DataFrame({'col1': [1, 2, 3], 'col2': [4, 5, 6]})
-
 
     gd = GridOptionsBuilder.from_dataframe(df)
     gd.configure_selection(selection_mode='multiple', use_checkbox=True)
@@ -91,8 +107,6 @@ with st.expander('Update bio and performance data'):
 
 with st.expander('Add range of motion data'):
     st.text('Add a range of motion data')
-
-
 
 # Score tab
 
